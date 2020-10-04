94c1644f910e28d5ef1cb64569ff84a7de3b9465
cre-pome <pometaka91@gmail.com>
2020-10-04 23:37:56 +0900

    タブを新規に作成して登録するよう修正

--- SpendingReport/app/src/main/java/com/example/spendingreport/MainActivity.kt
+++ SpendingReport/app/src/main/java/com/example/spendingreport/MainActivity.kt
@@ -6,19 +6,30 @@ import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import com.example.spendingreport.ui.main.SectionsPagerAdapter
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+
         val viewPager: ViewPager = findViewById(R.id.view_pager)
-        viewPager.adapter = sectionsPagerAdapter
+
         val tabs: TabLayout = findViewById(R.id.tabs)
+
+        // 「今月の出費記録」タブ
+        val thisMonthReport = tabs.newTab()
+        thisMonthReport.text = getString(R.string.tab_this_month_report)
+
+        // 「出費履歴」タブ
+        val spendHistoryTab = tabs.newTab()
+        spendHistoryTab.text = getString(R.string.tab_spend_history)
+
+        tabs.addTab(thisMonthReport)
+        tabs.addTab(spendHistoryTab)
+
+
         tabs.setupWithViewPager(viewPager)
         val fab: FloatingActionButton = findViewById(R.id.fab)
 
