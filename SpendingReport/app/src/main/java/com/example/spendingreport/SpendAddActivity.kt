4cc42e562f9d9952b8bac2741c663ccb99f09751
cre-pome <pometaka91@gmaail.com>
2020-10-20 19:42:19 +0900

    日付登録ダイアログが表示されるように変更

--- SpendingReport/app/src/main/java/com/example/spendingreport/SpendAddActivity.kt
+++ SpendingReport/app/src/main/java/com/example/spendingreport/SpendAddActivity.kt
@@ -1,5 +1,6 @@
 package com.example.spendingreport
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,6 +9,7 @@ import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
 import com.example.spendingreport.ui.main.TimePickerFragment
+import kotlinx.android.synthetic.main.activity_spend_add.*
 
 class SpendAddActivity : AppCompatActivity() , TimePickerFragment.OnTimeSelectedListener{
     private val spinnerFirstIndex = 5
@@ -18,9 +20,13 @@ class SpendAddActivity : AppCompatActivity() , TimePickerFragment.OnTimeSelected
         setContentView(R.layout.activity_spend_add)
 
         //　時刻入力テキスト
-        val timeSetText: TextView = findViewById(R.id.timeSetText)
         timeSetText.setOnClickListener{
             TimePickerFragment().show(supportFragmentManager, "timePicker")
+        }
+
+        //　日付入力テキスト
+        dateSetText.setOnClickListener{
+            showDatePicker()
         }
 
         // スピナーに値を入れる
@@ -43,6 +49,18 @@ class SpendAddActivity : AppCompatActivity() , TimePickerFragment.OnTimeSelected
     // 戻るボタン
     fun onButtonTapped(view: View?){
         finish()
+    }
+
+    private fun showDatePicker() {
+        val datePickerDialog = DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener() {view, year, month, dayOfMonth->
+                dateSetText.text = "${year}/${month + 1}/${dayOfMonth}"
+            },
+            2020,
+            3,
+            1)
+        datePickerDialog.show()
     }
 
     override fun onSelected(hourOfDay: Int, minute: Int) {
